e329811b5bdce20e51eb536a7dd8bb64eac9c371
Giuseppe Coppolecchia <g.coppolecchia2@gmail.com>
2026-01-28 11:21:39 +0100

    Initial commit - Landing page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar">
+    <div class="nav-brand">
+        {{-- <i class="fa-solid fa-record-vinyl text-white fa-2x"></i> --}}
+        <i class="nav-brand-icon"></i>
+        <span class="brand-text">BETH</span>
+    </div>
+
+    <div class="nav-menu mt-3">
+        <a class="nav-link" href="">MUSIC</a>
+        <a class="nav-link" href="">TOUR</a>
+        <a class="nav-link" href="">STORE</a>
+        <button class="nav-btn-icon">
+            <i class="fa-solid fa-bars text-white"></i>
+        </button>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+    <div class="container-fluid p-0 m-0 overflow-hidden">
+        <div class="row">
+            <div class="col-12 col-md-12 vh-100 d-flex justify-content-center align-items-center">
+                <video autoplay="autoplay" muted="muted" loop="loop" class="video-bg">
+                    <source src="https://cdn.pixabay.com/video/2020/12/03/58193-487176852_large.mp4" type="video/mp4">
+                    </video>
+                     <div class="content">
+                    <h1 class="main-text">FAUNA <br>
+                        <span class="text-second glitch" data-text="FLORA">FLORA</span></h1>
+                    <h2 class="subtitle pulse">New Album Out Now</h2>
+
+                        <div class="my-5 gap-4 d-flex flex-column flex-md-row justify-content-center align-items-center">
+                            <button class="btn-home">Listen Album <i class="fa-solid fa-play"></i></button>
+                            <button class="btn-search">Buy Now <i class="fa-solid fa-cart-shopping"></i></button>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+                    
+                    
+                </div>
+               
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        </x-layout>
